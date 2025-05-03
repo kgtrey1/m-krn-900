c492a17c2810403a3ba7f63df8434ce9ac939493
KG <kevin.gouyet@protonmail.com>
2025-05-03 05:45:03 +0000

    add: seeding for dictionary

---
+++ seed.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+
+echo "Seeding OTP dictionary with fruits..."
+./config/otp_config dict add Apple
+./config/otp_config dict add Banana
+./config/otp_config dict add Cherry
+./config/otp_config dict add Date
+./config/otp_config dict add Elderberry
+./config/otp_config dict add Fig
+./config/otp_config dict add Grape
+./config/otp_config dict add Honeydew
+./config/otp_config dict add Kiwi
+./config/otp_config dict add Lemon
+./config/otp_config dict add Mango
+./config/otp_config dict add Nectarine
+./config/otp_config dict add Orange
+./config/otp_config dict add Papaya
+./config/otp_config dict add Quince
+./config/otp_config dict add Raspberry
+./config/otp_config dict add Strawberry
+./config/otp_config dict add Tangerine
+./config/otp_config dict add Ugli
+./config/otp_config dict add Voavanga
+./config/otp_config dict add Watermelon
+./config/otp_config dict add Xigua
+./config/otp_config dict add Passionfruit
+./config/otp_config dict add Zucchini
+./config/otp_config dict add Apricot
+./config/otp_config dict add Blackberry
+./config/otp_config dict add Blueberry
+./config/otp_config dict add Cantaloupe
+./config/otp_config dict add Currant
+./config/otp_config dict add Dragonfruit
+./config/otp_config dict add Gooseberry
+./config/otp_config dict add Grapefruit
+./config/otp_config dict add Guava
+./config/otp_config dict add Jackfruit
+./config/otp_config dict add Jabuticaba
+./config/otp_config dict add Kumquat
+./config/otp_config dict add Lime
+./config/otp_config dict add Lychee
+./config/otp_config dict add Mandarin
+./config/otp_config dict add Mulberry
+./config/otp_config dict add Olive
+./config/otp_config dict add Peach
+./config/otp_config dict add Pear
+./config/otp_config dict add Persimmon
+./config/otp_config dict add Pineapple
+./config/otp_config dict add Plantain
+./config/otp_config dict add Plum
+./config/otp_config dict add Pomegranate
+./config/otp_config dict add Rambutan
+./config/otp_config dict add Soursop
+echo "Seeding complete."
